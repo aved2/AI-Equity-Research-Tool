e51364b3630547fc2b4839f1b98e910d63332f4f
Aditya Ved <63937430+aved2@users.noreply.github.com>
2024-06-06 02:32:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import faiss
 from dotenv import load_dotenv
