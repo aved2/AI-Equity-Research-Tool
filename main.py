3fb2baa6865501c87b7a878ccea4562b4163f98b
Aditya Ved <63937430+aved2@users.noreply.github.com>
2024-06-10 04:04:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ if query:
                         st.write(source)
             except Exception as e:
                 st.error("An error occurred. Please try a different URL.")
-                print(e)
+                st.write(e)
     
 
 
